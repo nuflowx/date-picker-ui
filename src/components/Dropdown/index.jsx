6d3d8544e7465993782ca184778015cbc8c3342c
nuflowx <>
2020-10-19 23:33:38 +0100

    Add dayjs and create a simple-dropdown to select the date
    Using dayjs to try out a new library, very straightfoward. Added a simple dropdown for date-select, adding time-select next.

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -4,23 +4,25 @@ import createBemClass from 'react-bem-builder';
 const cn = createBemClass('dropdown');
 
 function Dropdown(props) {
-  const [focused, setFocused] = React.useState(false);
-  const [value, setValue] = React.useState('');
-
   const {
     disabled,
     name,
     options,
   } = props;
 
+  const defaultOption = options.find((option) => option.selected === true) || {};
+
+  const [focused, setFocused] = React.useState(false);
+  const [value, setValue] = React.useState(defaultOption.value);
+
   return (
     <select
-      className={cn({ focused })}
+      className={cn('', { focused })}
       disabled={disabled}
       id={name}
       name={name}
       onBlur={() => setFocused(false)}
-      onChange={setValue}
+      onChange={({ target }) => setValue(target.value)}
       onFocus={() => setFocused(true)}
       value={value}
     >
--- src/components/SelectDate/index.jsx
+++ src/components/SelectDate/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 import Dropdown from '../Dropdown';
 
-function DateSelect() {
-  const options = [{ // change this to date-values
+function DateSelect() { // change this to scrollable buttons instead of select-dropdown?
+  const options = [{
     value: '',
-  }, {
-    value: 1,
-  }, {
-    value: 2,
   }];
+
+  let n = 0;
+  while (n <= 28) {
+    const day = dayjs().add(n, 'day');
+    day.add(n, 'day');
+    options.push({
+      value: day.format('YYYY-MM-DD'),
+      label: day.format('D ddd MMM'),
+      selected: day.isSame(dayjs(), 'day'),
+    });
+    n += 1;
+  }
 
   return (
     <div>
