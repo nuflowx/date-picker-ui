5ae326b401c4d71763b8c84797ac637e88f6fba6
nuflowx <>
2020-10-20 22:53:21 +0100

    Update time-slot in redux-state
    Dispatch an action to set the selected time in redux

--- src/components/SelectTime/TimeSlot.jsx
+++ src/components/SelectTime/TimeSlot.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import createBemClass from 'react-bem-builder';
+
 import './SelectTime.scoped.scss';
+
+const cn = createBemClass('time-slot');
 
 function TimeSlot(props) {
   const {
+    onClick = () => {},
     value,
+    selected,
   } = props;
 
   return (
-    <div className="time-slot">
+    <button type="button" className={cn('', { selected })} onClick={() => onClick(value)}>
       {value}
-    </div>
+    </button>
   );
 }
 
--- src/components/SelectTime/index.jsx
+++ src/components/SelectTime/index.jsx
@@ -1,39 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { SET_TIME } from '../../actions/types/new-booking-action-types';
 import getTimeSlots from '../../utilities/time-slot-utility'; // TODO: add aliases to not need relative paths
 
 import TimeSlot from './TimeSlot';
 import './SelectTime.scoped.scss';
 
-function SelectTime() {
+function SelectTime({ dispatch, selectedTime }) {
   // TODO: need to add/pass-through selected day-value
   // to be able to disable any past time-slots
   const morningSlots = getTimeSlots(6, 15, 24);
   const afternoonSlots = getTimeSlots(12, 15, 20);
   const eveningSlots = getTimeSlots(17, 15, 20);
 
+  const onClick = (time) => {
+    dispatch({ type: SET_TIME, payload: { time } });
+  };
+
   return (
     <div className="select-time__container">
       <div className="select-time__daytime-header">Morning</div>
       <div className="select-time__grid">
         {morningSlots.map((slot) => (
-          <TimeSlot value={slot.value} key={slot.value} /> // TODO: add disabled, selected & onClick
+          <TimeSlot
+            value={slot.value}
+            key={slot.value}
+            selected={slot.value === selectedTime}
+            onClick={onClick}
+          />
         ))}
       </div>
       <div className="select-time__daytime-header">Afternoon</div>
       <div className="select-time__grid">
         {afternoonSlots.map((slot) => (
-          <TimeSlot value={slot.value} key={slot.value} />
+          <TimeSlot
+            value={slot.value}
+            key={slot.value}
+            selected={slot.value === selectedTime}
+            onClick={onClick}
+          />
         ))}
       </div>
       <div className="select-time__daytime-header">Evening</div>
       <div className="select-time__grid">
         {eveningSlots.map((slot) => (
-          <TimeSlot value={slot.value} key={slot.value} />
+          <TimeSlot
+            value={slot.value}
+            key={slot.value}
+            selected={slot.value === selectedTime}
+            onClick={onClick}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-export default SelectTime;
+const mapStateToProps = (state) => ({
+  selectedTime: state.newBooking.time,
+});
+
+export default connect(mapStateToProps)(SelectTime);
